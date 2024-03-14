3bce98b5d3b7b23d8b7c9b17c7e050dcdbb59169
Anyzaikova <Anyzaikova@gmail.com>
2024-03-14 15:40:33 +0300

    init: start task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+    AppRoot,
+    SplitLayout,
+    SplitCol,
+    View,
+    Panel,
+    PanelHeader,
+    Header,
+    Group,
+    SimpleCell,
+    usePlatform,
+} from '@vkontakte/vkui';
+import '@vkontakte/vkui/dist/vkui.css';
+import {ConfigProvider, AdaptivityProvider} from '@vkontakte/vkui';
+import PanelTest from "./components/Panel/PanelTest";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const platform = usePlatform ();
+
+    return (
+        <ConfigProvider>
+            <AdaptivityProvider>
+                <AppRoot>
+                    <SplitLayout header={platform !== 'vkcom' && <PanelHeader />}>
+                        <SplitCol autoSpaced>
+                            <View activePanel="main">
+                                <Panel id="main">
+                                    <PanelHeader />
+                                    <Group header={<Header mode="secondary">Тестовое задание</Header>}>
+                                        <SimpleCell>
+                                            <PanelTest />
+                                        </SimpleCell>
+                                    </Group>
+                                </Panel>
+                            </View>
+                        </SplitCol>
+                    </SplitLayout>
+                </AppRoot>
+            </AdaptivityProvider>
+        </ConfigProvider>
+    );
+};
 
 export default App;

c0ba443d51d00aecb2460b52ac0d375f2634484c
esporov <esporov@phoenixit.ru>
2024-03-15 22:32:43 +0300

    feat: добавила возможность запуска в вк

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { ConfigProvider, AdaptivityProvider } from '@vkontakte/vkui';
+import bridge from "@vkontakte/vk-bridge";
+
+bridge.send("VKWebAppInit");
 
 ReactDOM.render(
     <ConfigProvider>
